e7d3830ac7fe7036bf2be5a5020f829640865d43
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-20 01:29:39 +0200

    1222

--- simple_shell.c
+++ simple_shell.c
@@ -1,41 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 #define BUF_SIZE 100
 
-/**
- * Displays the shell prompt.
- */
+/* Function prototypes */
 void display_prompt(void);
+void execute_command(char *command);
 
 /**
- * The main function for the simple shell.
+ * main - Entry point of the program
  *
- * Returns: Always 0.
+ * Return: Always 0 (success)
  */
-int main(void);
-
-void display_prompt(void)
+int main(void)
 {
-printf("#cisfun$ ");
-}
-
-int main(void){
 char command[BUF_SIZE];
 pid_t pid;
 int status;
 
-while (1){
+while (1)
+{
 display_prompt();
 
-if (fgets(command, BUF_SIZE, stdin) == NULL){
-if (feof(stdin)){
+if (fgets(command, BUF_SIZE, stdin) == NULL)
+{
+if (feof(stdin))
+{
 printf("\n");
 break;
-} else {
+}
+else
+{
 perror("fgets");
 continue;
 }
@@ -43,27 +41,63 @@ continue;
 
 command[strcspn(command, "\n")] = '\0';
 
-if (strlen(command) == 0){
+if (strlen(command) == 0)
+{
 continue;
 }
 
 pid = fork();
-if (pid == -1){
+if (pid == -1)
+{
 perror("fork");
 continue;
-} else if (pid == 0){
-char *args[] = {"/bin/sh", "-c", command, NULL};
-execve("/bin/sh", args, NULL);
-perror("execve");
+}
+else if (pid == 0)
+{
+execute_command(command);
 exit(EXIT_FAILURE);
-} else {
+}
+else
+{
 waitpid(pid, &status, 0);
 
-if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE){
+if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE)
+{
 printf("%s: No such file or directory\n", command);
 }
 }
 }
 
-return 0;
+return (0);
+}
+
+/**
+ * display_prompt - Displays the shell prompt
+ */
+void display_prompt(void)
+{
+printf("#cisfun$ ");
+}
+
+/**
+ * execute_command - Executes the given command using execve
+ * @command: The command to be executed
+ */
+void execute_command(char *command)
+{
+char **args = malloc(4 * sizeof(char *));
+if (args == NULL)
+{
+perror("malloc");
+exit(EXIT_FAILURE);
+}
+
+args[0] = "/bin/sh";
+args[1] = "-c";
+args[2] = command;
+args[3] = NULL;
+
+execve("/bin/sh", args, NULL);
+perror("execve");
+exit(EXIT_FAILURE);
 }
