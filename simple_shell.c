2107bbf9cf5bd3e431b5e55c3b1fdddbc7a54ce7
Moe-95 <mhammad_fahs@hotmail.com>
2023-12-21 20:30:18 +0200

    last check

--- simple_shell.c
+++ simple_shell.c
@@ -601,46 +601,51 @@ int handle_prompt(size_t *length, char **line)
  */
 int check_builtins(int cnt, char **tokens, int *exit_status, char **argv)
 {
-	char **env = environ;
-	char newline = '\n';
+    char newline = '\n';
 
-	if (cnt < 1)
-	{
-		return (2);
-	}
-	if (strcmp(tokens[0], "exit") == 0)
-	{
-		if (tokens[1] != NULL)
-		{
-			if (atoi(tokens[1]))
-				if (atoi(tokens[1]) >= 0)
-					*exit_status = _atoi(tokens[1]);
-				else
-					error_message(tokens, argv, exit_status);
-			else
-			{
-				error_message(tokens, argv, exit_status);
-			}
-		}
-		return (1);
-	}
-	if (strcmp(tokens[0], "env") == 0)
-	{
-		env = environ;
-		while (*env != NULL)
-		{
-			write(STDOUT_FILENO, *env, strlen(*env));
-			write(STDOUT_FILENO, &newline, 1);
-			env++;
-		}
-		return (2);
-	}
-	if (strcmp(tokens[0], "unsetenv") == 0)
-	{
-		_unsetenv(tokens[1]);
-		return (2);
-	}
-	return (0);
+    if (cnt < 1)
+    {
+        return 2;
+    }
+
+    if (strcmp(tokens[0], "exit") == 0)
+    {
+        if (tokens[1] != NULL)
+        {
+            if (atoi(tokens[1]))
+            {
+                if (atoi(tokens[1]) >= 0)
+                    *exit_status = _atoi(tokens[1]);
+                else
+                    error_message(tokens, argv, exit_status);
+            }
+            else
+            {
+                error_message(tokens, argv, exit_status);
+            }
+        }
+        return 1;
+    }
+
+    if (strcmp(tokens[0], "env") == 0)
+    {
+        char **env = environ;
+        while (*env != NULL)
+        {
+            write(STDOUT_FILENO, *env, strlen(*env));
+            write(STDOUT_FILENO, &newline, 1);
+            env++;
+        }
+        return 2;
+    }
+
+    if (strcmp(tokens[0], "unsetenv") == 0)
+    {
+        _unsetenv(tokens[1]);
+        return 2;
+    }
+
+    return 0;
 }
 
 
